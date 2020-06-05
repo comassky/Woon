96d2490f989cfddb983ee3107cf7594a976e2a4d
Hadrien JACQUOT <jacquot.hadrien@gmail.com>
2020-06-05 22:07:48 +0200

    Fix tree

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,67 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+  <v-app id="inspire">
+    <Menu />
+    <v-app-bar app clipped-left>
+      <v-app-bar-nav-icon @click.stop="drawer = !drawer"></v-app-bar-nav-icon>
+      <v-toolbar-title>Application</v-toolbar-title>
+    </v-app-bar>
+
+    <v-content>
+      <v-container class="fill-height" fluid>
+        <v-row align="center" justify="center">
+          <v-col class="shrink">
+            <Zone
+              v-for="zone in zones"
+              :zone="zone"
+              :key="zone.id_zone"
+              @volumeUpdate="updateVolume"
+            />
+          </v-col>
+        </v-row>
+      </v-container>
+    </v-content>
+
+    <v-footer app>
+      <span>&copy; 2020</span>
+    </v-footer>
+  </v-app>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Zone from "./components/Zone";
+import Menu from "./components/Menu";
+
+const RoonServices = require("./services/roonServices");
 
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
-</script>
+  name: "App",
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
+  components: {
+    Zone,
+    Menu
+  },
+
+  data: () => ({
+    zones: [],
+    selectedZone: {},
+    myRoonInstance: {}
+  }),
+
+  created() {
+    this.$vuetify.theme.dark = true;
+  },
+
+  mounted() {
+    this.myRoonInstance = new RoonServices();
+    this.myRoonInstance.connectToRoon().then(() => {
+      this.zones = this.myRoonInstance.getRoonZones();
+    });
+  },
+
+  methods: {
+    updateVolume(volumeInfo) {
+      this.myRoonInstance.updateVolume(volumeInfo.output, volumeInfo.volume);
+    }
+  }
+};
+</script>
